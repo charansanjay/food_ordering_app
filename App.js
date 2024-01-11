e2db4395feacc2bc0cac6326d318e21a230bcbd5
charansanjay <37692058+charansanjay@users.noreply.github.com>
2024-01-11 14:38:24 +0100

    Initialized React App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Main Top level component
+const AppLayout = () => {
+  return (
+    <div>
+      {/* Header */}
+      {/* Body */}
+      {/* Footer */}
+    </div>
+  )
+}
